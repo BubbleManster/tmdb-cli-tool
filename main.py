c563dc2d4b76c45211dc2fb5a835a8b9089b3f34
Dylan Lewis <dylanlewis11@icloud.com>
2025-03-20 21:19:56 +0000

    Added all other commands and functionality.

--- main.py
+++ main.py
@@ -18,19 +18,29 @@ try:
         match currentValue:
             case "playing":
                 url = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"
-
-                headers = {
-                    "accept": "application/json",
-                    "Authorization": f"Bearer {API_TOKEN}"
-                }
-
-                response = requests.get(url, headers=headers)
-                data = response.json()
-
-                print("Films currently in theatres:")
-                for film in data['results']:
-                    print(" - " + film['original_title'])
+                category = "in cinemas now"
+            case "popular":
+                url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+                category = "popularity"
+            case "top":
+                url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"
+                category = "highest rating"
+            case "upcoming":
+                url = "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1"
+                category = "soon to be released"
             
 except getopt.error as err:
     # output error, and return with an error code
     print (str(err))
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {API_TOKEN}"
+}
+
+response = requests.get(url, headers=headers)
+data = response.json()
+
+print(f"Ranking films based on {category}:")
+for i, film in enumerate(data['results']):
+    print(f"{i + 1}. " + film['original_title'])
