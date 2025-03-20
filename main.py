4e77bbf69ed2bc7e2e7b08463b38e0f7f797ba05
Dylan Lewis <dylanlewis11@icloud.com>
2025-03-20 21:06:56 +0000

    Improved command checking functionality.
    Replaced if/else with a match/case

--- main.py
+++ main.py
@@ -15,20 +15,21 @@ try:
     arguments, values = getopt.getopt(argumentList, "", long_options)
     # checking each argument
     for currentArgument, currentValue in arguments:
-        if currentValue == "playing":
-            url = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"
+        match currentValue:
+            case "playing":
+                url = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"
 
-            headers = {
-                "accept": "application/json",
-                "Authorization": f"Bearer {API_TOKEN}"
-            }
+                headers = {
+                    "accept": "application/json",
+                    "Authorization": f"Bearer {API_TOKEN}"
+                }
 
-            response = requests.get(url, headers=headers)
-            data = response.json()
+                response = requests.get(url, headers=headers)
+                data = response.json()
 
-            print("Films currently in theatres:")
-            for film in data['results']:
-                print(" - " + film['original_title'])
+                print("Films currently in theatres:")
+                for film in data['results']:
+                    print(" - " + film['original_title'])
             
 except getopt.error as err:
     # output error, and return with an error code
